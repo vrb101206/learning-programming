0bfcbbbf02dd06e0973f7483cd2399b8eb014a7c
vrb101206 <vrosero101@gmail.com>
2026-02-26 22:18:43 -0500

    Update data_types.py

--- paython/data_types.py
+++ paython/data_types.py
@@ -1,1 +1,14 @@
+#this is my first python Script  
+''' 
+Developer: "victor R."
+Description: this script let us understand  how to is data types.
+'''
 
+print("hello everyone- welcome to data types class")
+
+age = 17 
+print(type8(age))
+EARTH_GRAVITY = 9.8
+print(types(EARTH_GRAVITY))
+PI = 3.1416
+print(types(PI))
